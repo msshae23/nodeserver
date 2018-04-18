e0e2d7a27a00ac1dcf751f1a20ca5012ef666524
Shaela Sterling <msshae23_09@yahoo.com>
2018-04-18 19:34:53 -0400

    new vars

--- mean.js
+++ mean.js
@@ -1,13 +1,13 @@
 const http = require("http");
-const PORT = 7500;
+const PORT2 = 7500;
 var server = http.createServer(function(req, res){
 	res.end("Hello! I'm the tell you something awful URL: " + req.url);
 });
 
-server.listen(PORT, function(err){
+server.listen(PORT2, function(err){
 	if(err){
 		console.error("Couldn't connect, sorry..kinda", err);
 	}else {
-		console.log("We are now listening on port " + PORT + " and everyone hates you!")
+		console.log("We are now listening on port " + PORT2 + " and everyone hates you!")
 	}
 });
--- nice.js
+++ nice.js
@@ -1,13 +1,13 @@
 const http = require("http");
-const PORT = 7000;
+const PORT1 = 7000;
 var server = http.createServer(function(req, res){
 	res.end("Hello! I'm the tell you something good URL: " + req.url);
 });
 
-server.listen(PORT, function(err){
+server.listen(PORT1, function(err){
 	if(err){
 		console.error("Couldn't connect, sorry..kinda", err);
 	}else {
-		console.log("We are now listening on port " + PORT + " and you are the sweetest person!")
+		console.log("We are now listening on port " + PORT1 + " and you are the sweetest person!")
 	}
 });
